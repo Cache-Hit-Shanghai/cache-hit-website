f5bc07ee122183057b76b51fe7ad28c887a63d3c
Jim Jin <jim_king_2000@163.com>
2023-07-31 18:40:14 +0800

    add aiot pass

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box, Heading, Text, Image, Stack, Diagram, Button, Card, CardHeader, CardBody } from 'grommet';
+import { Box, Heading, Text, Image, Stack, Diagram, Card, CardHeader, CardBody, Grid } from 'grommet';
 import { Cloud, Webcam, Checkmark } from 'grommet-icons';
 
 export default function Page() {
@@ -11,7 +11,7 @@ export default function Page() {
           <Box gap='medium'>
             <Heading margin='none' size='large' color='neutral-1'>雎鸠云</Heading>
             <Heading margin='none' color='neutral-1'>AIoT PaaS平台</Heading>
-            <Heading margin='none' level={2} color='accent-4'>为万物自治而努力！</Heading>
+            <Heading margin='none' level={2} color='accent-4'>构建万物自治的世界！</Heading>
           </Box>
           <Text>领先的纯H5 AIoT PaaS平台与端云一体的行业解决方案。将AIoT产品落地事件缩短至1个月，开发成本缩短至原来的十分之一。全网页控制端，原生支持几乎所有的操作系统。基于零信任网络打造，全栈加密，充分保障系统安全。</Text>
         </Box>
@@ -79,7 +79,7 @@ export default function Page() {
             <Card background='background-front' width='medium'>
               <CardHeader background='active' pad='small'>
                 <Box direction='row' align='center' gap='small'>
-                  <Button plain primary icon={<Checkmark />} />
+                  <Box round background='brand'><Checkmark /></Box>
                   <Heading level={4} margin='none'>超高安全性</Heading>
                 </Box>
               </CardHeader>
@@ -90,7 +90,7 @@ export default function Page() {
             <Card background='background-front' width='medium'>
               <CardHeader background='active' pad='small'>
                 <Box direction='row' align='center' gap='small'>
-                  <Button plain primary icon={<Checkmark />} />
+                  <Box round background='brand'><Checkmark /></Box>
                   <Heading level={4} margin='none'>P2P传输</Heading>
                 </Box>
               </CardHeader>
@@ -101,7 +101,7 @@ export default function Page() {
             <Card background='background-front' width='medium'>
               <CardHeader background='active' pad='small'>
                 <Box direction='row' align='center' gap='small'>
-                  <Button plain primary icon={<Checkmark />} />
+                  <Box round background='brand'><Checkmark /></Box>
                   <Heading level={4} margin='none'>全互联网化</Heading>
                 </Box>
               </CardHeader>
@@ -111,6 +111,97 @@ export default function Page() {
             </Card>
           </Box>
         </Box>
+      </Box>
+      <Box width='xxlarge' pad='large' background='linear-gradient(to bottom right, #00873D, #00739D)'>
+        <Heading level={2}>雎鸠云Web AIoT PaaS平台</Heading>
+        <Grid
+          rows={['auto', 'auto', 'auto', 'auto', 'auto', 'auto']}
+          columns={['auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto']}
+          areas={[
+            { name: 'paas_title', start: [0, 2], end: [0, 5] },
+            { name: 'web_paas', start: [1, 2], end: [2, 5] },
+            { name: 'ai_paas', start: [3, 2], end: [3, 5] },
+            { name: 'iot_paas', start: [4, 2], end: [5, 5] },
+            { name: 'fw_paas', start: [6, 2], end: [6, 5] },
+            { name: 'hw_paas', start: [7, 2], end: [7, 5] },
+          ]}
+        >
+          <Box>场景</Box>
+          <Box>智慧交通</Box>
+          <Box>智慧楼宇</Box>
+          <Box>智慧家居</Box>
+          <Box>智慧城市</Box>
+          <Box>智慧零售</Box>
+          <Box>智慧校园</Box>
+          <Box>智慧办公</Box>
+          <Box>SaaS</Box>
+          <Box>视频监控平台</Box>
+          <Box>位置信息平台</Box>
+          <Box>智能家居平台</Box>
+          <Box>智慧照明平台</Box>
+          <Box>可穿戴平台</Box>
+          <Box>智能网络平台</Box>
+          <Box>数字公路平台</Box>
+          <Box gridArea='paas_title'>PaaS</Box>
+          <Grid
+            gridArea='web_paas'
+            rows={['auto', 'auto', 'auto', 'auto']}
+            columns={['auto', 'auto']}
+            areas={[{ name:'web_paas_title', start: [0, 0], end: [1, 0] }]}
+          >
+            <Box gridArea='web_paas_title'>Web</Box>
+            <Box>登录</Box>
+            <Box>音视频</Box>
+            <Box>支付</Box>
+            <Box>Billing</Box>
+            <Box>数据分析</Box>
+            <Box>消息分发</Box>
+          </Grid>
+          <Grid
+            gridArea='ai_paas'
+            rows={['auto', 'auto', 'auto', 'auto']}
+            columns={['auto']}
+          >
+            <Box>AI</Box>
+            <Box>人脸识别</Box>
+            <Box>手势识别</Box>
+            <Box>车牌识别</Box>
+          </Grid>
+          <Grid
+            gridArea='iot_paas'
+            rows={['auto', 'auto', 'auto', 'auto']}
+            columns={['auto', 'auto']}
+            areas={[{ name:'iot_paas_title', start: [0, 0], end: [1, 0] }]}
+          >
+            <Box gridArea='iot_paas_title'>IoT</Box>
+            <Box>MQTT</Box>
+            <Box>设备管理</Box>
+            <Box>时序数据库</Box>
+            <Box>远程诊断</Box>
+            <Box>位置与传感</Box>
+            <Box>OTA</Box>
+          </Grid>
+          <Grid
+            gridArea='fw_paas'
+            rows={['auto', 'auto', 'auto', 'auto']}
+            columns={['auto']}
+          >
+            <Box>固件</Box>
+            <Box>SDK</Box>
+            <Box>GUI</Box>
+            <Box>多语言运行时</Box>
+          </Grid>
+          <Grid
+            gridArea='hw_paas'
+            rows={['auto', 'auto', 'auto', 'auto']}
+            columns={['auto']}
+          >
+            <Box>硬件</Box>
+            <Box>核心板</Box>
+            <Box>通讯模组</Box>
+            <Box>视觉模组</Box>
+          </Grid>
+        </Grid>
       </Box>
       <Box width='xxlarge' pad={{ vertical: 'small', horizontal: 'large'}} background='background-front' direction='row' gap='medium'>
         <Text size='small' color='text-weak'>&copy;2020-2023 上海缓存命中科技有限公司 版权所有</Text>
